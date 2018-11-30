f2a16df28d1b9463b2b37056c9a7eccb6c3fe859
arnau <arnau@DESKTOP-4NC1U8A>
2018-11-30 11:48:51 +0100

    Creacion del Front-End definitivo de:
    Pagina inicial (LOGIN)
    Pagina de REGISTRO

--- registro/index.php
+++ registro/index.php
@@ -5,9 +5,9 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="favicon.ico"/>
+	<link rel="icon" type="image/png" href="../favicon.ico"/>
 <!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="assets/css/inicio-sesion/style.css">
+	<link rel="stylesheet" type="text/css" href="../assets/css/registro/style.css">
 <!--===============================================================================================-->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 <!--===============================================================================================-->
@@ -17,13 +17,32 @@
 		<div class="todo">
 			<div class="caja">	
 			<form class="formulario" action="" method="post" >
-					<span class="titulo">AOM</span> 
-					<span class="sub-titulo">Versión 0.1</span>
+					<span class="titulo">Registro</span> 
+					<span class="sub-titulo">AOM Subscription Manager</span>
 				 <span class="sub-titulo"></span>
+				 <div class="input" >
+						<input class="caja-input" type="text" id="nombre" name="nombre" autocomplete="new-password" placeholder="Nombre"  required>
+						<span class="icono">
+							<i class="fa fa-user-circle" aria-hidden="true"></i>
+						</span>
+					</div>
+					<div class="input" >
+						<input class="caja-input" type="text" id="apellidos" name="apellidos" autocomplete="new-password" placeholder="Apellidos"  required>
+						<span class="icono">
+							<i class="fa fa-user-circle" aria-hidden="true"></i>
+						</span>
+					</div>
+					<div class="input" >
+						<input class="caja-input" type="date" id="edad" name="edad" autocomplete="new-password" placeholder="Fecha nacimiento"  required>
+						<span class="icono">
+						<i class="fas fa-birthday-cake"></i>
+						</span>
+					</div>
+					
 					<div class="input" >
 						<input class="caja-input" type="text" id="email" name="email" autocomplete="new-password" placeholder="Email"  required>
 						<span class="icono">
-							<i class="fa fa-user-circle" aria-hidden="true"></i>
+						<i class="fas fa-envelope"></i>
 						</span>
 					</div>
 					<div class="input" >
@@ -33,10 +52,16 @@
 						</span>
 					</div>
 					<div class="input" >
-				
+						<input class="caja-input" type="password" id="password2" name="password2" autocomplete="new-password" placeholder="Repite la contraseña" required>						
+						<span class="icono">
+						<i class="fas fa-lock-open"></i>
+						</span>
+					</div>
+
+					<div class="input" >
 					<div class="todo-form-btn">
 						<button type="submit" class="caja-titulo-btn">
-				ENTRAR
+				COMPLETAR
 						</button>
 					</div><br>
 				
@@ -44,11 +69,9 @@
 			
 				?>
 					<div class="text-center">
-						<a class="txt5" href="recuperar-password/">
-						 Recuperar contraseña  <i class="fa fa-key" aria-hidden="true"></i>
-						  </a><br>
-						  <a class="txt4" href="registro/">
-						  Registro <i class="fas fa-user-plus"></i>
+						
+						  <a class="txt4" href="../">
+						  <i class="fas fa-arrow-left"></i> Iniciar Sesión 
 						  </a><br><br>
 					
 						</div>
