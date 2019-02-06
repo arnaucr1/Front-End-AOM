cbb24bbae4db1e17598115b61433691cf102c4a5
Arnau Canal <arnau_131@hotmail.com>
2019-02-06 13:11:45 +0100

    Cambio de animacion y deshabilitadas todas las scrollbars

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,22 +13,36 @@ import {
   styleUrls: ['./app.component.css'],
   animations: [
 trigger('myAnimation', [
-  transition('* => *', [
-    query(
-      ':enter',
-      [style({ opacity: 0 })],
-      { optional: true }
-    ),
-    query(
-      ':leave',
-       [style({ opacity: 1 }), animate('0.1s', style({ opacity: 0 }))],
-      { optional: true }
-    ),
-    query(
-      ':enter',
-      [style({ opacity: 0 }), animate('0.1s', style({ opacity: 1 }))],
-      { optional: true }
-    )
+  transition('* <=> *', [
+    // Initial state of new route
+    query(':enter',
+      style({
+        position: 'fixed',
+        width:'100%',
+        transform: 'translateX(100%)'
+      }),
+      {optional:true}),
+
+    // move page off screen right on leave
+    query(':leave',
+      animate('300ms ease',
+        style({
+          position: 'fixed',
+          width:'100%',
+          transform: 'translateX(-100%)'
+        })
+      ),
+    {optional:true}),
+
+    // move page in screen from left to right
+    query(':enter',
+      animate('300ms ease',
+        style({
+          opacity: 1,
+          transform: 'translateX(0%)'
+        })
+      ),
+    {optional:true}),
   ])
 ]),
 
