66ea5eded8d8513a1c59a17a0880bbf1b13a117c
System Administrator <root@MacBook-Air-de-Ot.local>
2019-03-06 12:53:05 +0100

    Webservice ediar datos de perfil

--- src/app/editar-subscripcion/editar-subscripcion.component.ts
+++ src/app/editar-subscripcion/editar-subscripcion.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { SubscriptionService } from '../subscription.service';
+import { Subscription } from '../subscription';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-editar-subscripcion',
   templateUrl: './editar-subscripcion.component.html',
-  styleUrls: ['./editar-subscripcion.component.css']
+  styleUrls: ['./editar-subscripcion.component.css'],
+  providers: [SubscriptionService]
 })
 export class EditarSubscripcionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private subscriptionService:SubscriptionService) {}
 
   ngOnInit() {
   }
