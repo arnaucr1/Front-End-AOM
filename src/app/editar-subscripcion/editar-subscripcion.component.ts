371b60b0eb3a24f14643ddc57ce078381485cc22
System Administrator <root@MacBook-Air-de-Ot.local>
2019-05-17 13:13:47 +0200

    OT: Header página editar subscripción

--- src/app/editar-subscripcion/editar-subscripcion.component.ts
+++ src/app/editar-subscripcion/editar-subscripcion.component.ts
@@ -4,24 +4,41 @@ import { Subscription } from '../subscription';
 import { HttpClient } from '@angular/common/http';
 import { NotifierService } from 'angular-notifier';
 import {Router} from "@angular/router";
+import { UserService } from '../user.service';
+import { DatePipe } from '@angular/common';
+import {User} from '../user';
 
 @Component({
   selector: 'app-editar-subscripcion',
   templateUrl: './editar-subscripcion.component.html',
   styleUrls: ['./editar-subscripcion.component.css'],
-  providers: [SubscriptionService]
+  providers: [SubscriptionService, DatePipe]
 })
 export class EditarSubscripcionComponent implements OnInit {
+  today = new Date();
   private notifier: NotifierService;
-  constructor(private subscriptionService:SubscriptionService, notifier: NotifierService, private router: Router) {
+  userData:User[] = [];
+  userType = localStorage.getItem("type");
+
+  constructor(private subscriptionService:SubscriptionService, notifier: NotifierService, private router: Router, private datePipe: DatePipe, private userService:UserService) {
+    let dataActual = Date.now();
     this.notifier = notifier;
   }
-  userType = localStorage.getItem("type");
-  test = localStorage.getItem("firstName");
   
   editSubscription:Subscription = new Subscription(0, "", "", 0, null, 0, 0, null);
   ngOnInit() {
+    this.getU();
     this.getSubscription(parseInt(localStorage.getItem("subscriptionID")));
+  }
+
+  getU() {
+		this.userService.getUserToken().subscribe(
+			(result) => {
+				this.userData = result["data"];
+			}, (error) => {
+				this.notifier.notify('error','No hay ningún usuario logeado');
+			}
+		) 
   }
   
   getSubscription(subscriptionID:number) {
@@ -45,4 +62,5 @@ export class EditarSubscripcionComponent implements OnInit {
       }
     )
   }
+  
 }
