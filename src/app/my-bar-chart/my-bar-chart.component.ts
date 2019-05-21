d25099a1b0c5fefb2d2a9443ce78442171546675
System Administrator <root@MacBook-Air-de-Ot.local>
2019-05-21 10:05:05 +0200

    OT: Mostrar gráficas de coste de subscripciones divididas por el ciclo de pago

--- src/app/my-bar-chart/my-bar-chart.component.ts
+++ src/app/my-bar-chart/my-bar-chart.component.ts
@@ -15,6 +15,37 @@ export class MyBarChartComponent implements OnInit {
   constructor(private subscriptionService:SubscriptionService, private userService:UserService,notifier: NotifierService) { 
     this.notifier = notifier;
   }
+
+  public pricesMensual = [];
+  public barChartLabelsMensual = [];
+  public barChartDataMensual = [
+    {data: this.pricesMensual, label: 'Coste de cada subscripción en €'}
+  ];
+
+  public pricesTrimestral = [];
+  public barChartLabelsTrimestral = [];
+  public barChartDataTrimestral = [
+    {data: this.pricesTrimestral, label: 'Coste de cada subscripción en €'}
+  ];
+
+  public pricesSemestral = [];
+  public barChartLabelsSemestral = [];
+  public barChartDataSemestral = [
+    {data: this.pricesSemestral, label: 'Coste de cada subscripción en €'}
+  ];
+
+  public pricesAnual = [];
+  public barChartLabelsAnual = [];
+  public barChartDataAnual = [
+    {data: this.pricesAnual, label: 'Coste de cada subscripción en €'}
+  ];
+
+  public pricesBianual = [];
+  public barChartLabelsBianual = [];
+  public barChartDataBianual = [
+    {data: this.pricesBianual, label: 'Coste de cada subscripción en €'}
+  ];
+
   public barChartLabels = [];
   public prices = [];
   public barChartData = [
@@ -26,6 +57,7 @@ export class MyBarChartComponent implements OnInit {
   mySubscriptions:Subscription[] = [];
 
   ngOnInit() {
+    console.log(this.barChartLabelsBianual);
     this.getSubscriptions(parseInt(localStorage.getItem("userID")));
   }
 
@@ -35,8 +67,30 @@ export class MyBarChartComponent implements OnInit {
           this.mySubscriptions = result["data"];
           console.log(this.mySubscriptions);
           for (let subscription of this.mySubscriptions) {
-            this.barChartLabels.push(subscription.subscriptionName);
-            this.prices.push(subscription.price);
+            //console.log(subscription.cycle);
+            
+            if(subscription.cycle == 1) {
+              this.barChartLabelsMensual.push(subscription.subscriptionName);
+              this.pricesMensual.push(subscription.price);
+            }
+            if (subscription.cycle == 3) {
+              this.barChartLabelsTrimestral.push(subscription.subscriptionName);
+              this.pricesTrimestral.push(subscription.price);
+            }
+            if (subscription.cycle == 6) {
+              this.barChartLabelsSemestral.push(subscription.subscriptionName);
+              this.pricesSemestral.push(subscription.price);
+            }
+            if (subscription.cycle == 12) {
+              this.barChartLabelsAnual.push(subscription.subscriptionName);
+              this.pricesAnual.push(subscription.price);
+            }  
+            if (subscription.cycle == 24) {
+              this.barChartLabelsBianual.push(subscription.subscriptionName);
+              this.pricesBianual.push(subscription.price);
+            }
+            //this.barChartLabels.push(subscription.subscriptionName);
+            //this.prices.push(subscription.price);
           }
         }, (error) => {
           this.notifier.notify('error','Error al cargar las subscripciones');
